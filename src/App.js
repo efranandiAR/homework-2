53fd66a14d7f41af108df0431ed3392c7123eb14
efranandiAR <83424201+efranandiAR@users.noreply.github.com>
2022-03-23 15:40:56 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import data from './single-sample';
+
+const ListArtist = data.artists.map((element) =>
+  <li>{element.name}</li>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-title">
+          <h1>Music Player</h1>
+        </div>
       </header>
+      <div className="MusicPlayer">
+      <div className="Container">
+        <img
+          src={ data.album.images[1].url }
+          className="AlbumPicture"
+          alt=''
+        />
+        <ul className="SongDetails">
+          <li className="SongTitle">{ data.name }</li>
+          <li className="SongAlbum">{ data.album.name }</li>
+          <li className="SongArtists">
+            <ul>{ListArtist}</ul>
+          </li>
+        </ul>
+        <button className="Btn-Select">Select</button>
+      </div>
+    </div>
     </div>
   );
 }
